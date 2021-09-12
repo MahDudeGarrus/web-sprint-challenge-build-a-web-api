c3bffd100365a3940bbdb7d6bb5e713d42e23db6
Your Name <you@example.com>
2021-09-12 15:38:52 -0600

    set up the skeleton for actions and project routers

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,25 @@
-// Write your "actions" router here!
+const express = require("express")
+const router = express.Router();
+const Action = require('./actions-model.js')
+
+// ACTION ENDPOINTS
+
+router.get('/api/actions', (res, req) => {
+
+})
+
+router.get('/api/actions/:id', (res, req) => {
+
+})
+
+router.post('/api/actions', (res, req) => {
+
+})
+
+router.put('/api/actions/:id', (res, req) => {
+
+})
+
+router.delete('/api/actions/:id', (res, req) => {
+
+})
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,29 @@
-// Write your "projects" router here!
+const express = require("express")
+const router = express.Router();
+const Project= require('./projects-model.js');
+
+// PROJECT ENDPOINTS
+
+router.get('/api/projects', (res, req) => {
+
+})
+
+router.get('/api/projects/:id', (res, req) => {
+
+})
+
+router.post('/api/projects', (res, req) => {
+
+})
+
+router.put('/api/projects/:id', (res, req) => {
+
+})
+
+router.delete('/api/projects/:id', (res, req) => {
+
+})
+
+router.get('/api/projects/:id/actions', (res, req) => {
+
+})
